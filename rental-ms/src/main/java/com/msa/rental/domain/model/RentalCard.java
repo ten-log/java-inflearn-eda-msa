787de1ff1996660faafdc7aae5a18f532fe651a1
CHOIYEOL <choi_yeol @coconev.co.jp>
2024-03-28 15:54:26 +0900

    add: rental proccess business logic
    렌탈시스템의 대여, 처리등의 비지니스 로직 생성

--- rental-ms/src/main/java/com/msa/rental/domain/model/RentalCard.java
+++ rental-ms/src/main/java/com/msa/rental/domain/model/RentalCard.java
@@ -1,14 +1,12 @@
 package com.msa.rental.domain.model;
 
-import com.msa.rental.domain.model.vo.IDName;
-import com.msa.rental.domain.model.vo.RentStatus;
-import com.msa.rental.domain.model.vo.RentalCardNo;
-import com.msa.rental.domain.model.vo.ReturnItem;
-import com.msa.rental.domain.model.vo.LateFee;
+import com.msa.rental.domain.model.vo.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,6 +38,42 @@ public class RentalCard {
     }
     private void addReturnItem(ReturnItem returnItem){
         this.returnItemList.add(returnItem);
+    }
+
+    //대여카드 생성
+    public static RentalCard createRentalCard(IDName creator){
+        RentalCard rentalCard = new RentalCard();
+        rentalCard.setRentalCardNo(RentalCardNo.crateRentalCardNo());
+        rentalCard.setRentStatus(RentStatus.RENT_AVAILABLE);
+        rentalCard.setLateFee(LateFee.createLateFee());
+        return rentalCard;
+    }
+
+    //대여처리
+    public RentalCard rentItem(Item item){
+        checkRentalAvailable();
+        this.addRentalItem(RentalItem.createRentalItem(item));
+        return this;
+    }
+
+    private void checkRentalAvailable(){
+        if(this.rentStatus == RentStatus.RENT_UNAVILABLE) throw new IllegalArgumentException("대여불가입니다");
+        if(this.rentalItemList.size() >5) throw new IllegalArgumentException("이미 5원빌림 최대 대여수입니다");
+    }
+
+    public RentalCard returnItem(Item item, LocalDate returnDate){
+        RentalItem rentalItem = this.rentalItemList.stream().filter(i-> i.getItem().equals(item)).findFirst().get();
+        calculateLateFee(rentalItem, returnDate);
+        this.addReturnItem(ReturnItem.createReturnItem(rentalItem));
+        this.removeRentalItem(rentalItem);
+        return this;
+    }
+
+    private void calculateLateFee(RentalItem rentalItem, LocalDate returnDate) {
+        if(returnDate.compareTo(rentalItem.getOverdueDate()) > 0){
+            int point = Period.between(rentalItem.getOverdueDate(), returnDate).getDays()*10;
+            this.lateFee.addPoint(point);
+        }
     }
 
 }
