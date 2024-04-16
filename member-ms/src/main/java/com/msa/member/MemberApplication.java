93f5c63446414470d9040d20c25e1d769a0e9bfb
CHOIYEOL <choi_yeol @coconev.co.jp>
2024-04-16 17:02:19 +0900

    fix: 모든 서버 작동하게 변경

--- member-ms/src/main/java/com/msa/member/MemberApplication.java
+++ member-ms/src/main/java/com/msa/member/MemberApplication.java
@@ -16,26 +16,26 @@ public class MemberApplication {
         SpringApplication.run(MemberApplication.class, args);
     }
 
-//    public static void dominTest() throws Exception {
-//        System.out.println("------------도메인 모델 테스트 진행------------");
-//        System.out.println("회원 생성");
-//        var member = Member.registerMember(IDName.sample(), PassWord.sample(), Email.sample());
-//        System.out.println(member.toString());
-//
-//        System.out.println("포인트 10 적립");
-//        member.savePoint(new Point(10));
-//
-//        System.out.println(member.getPoint());
-//        System.out.println("포인트 20 적립");
-//        member.savePoint(new Point(20));
-//        System.out.println(member.getPoint());
-//
-//        System.out.println("포인트 10 사용");
-//        member.usePoint(new Point(10));
-//        System.out.println(member.getPoint());
-//
-//        System.out.println("-----------------------------------------");
-//
-//    }
+    public static void dominTest() throws Exception {
+        System.out.println("------------도메인 모델 테스트 진행------------");
+        System.out.println("회원 생성");
+        var member = Member.registerMember(IDName.sample(), PassWord.sample(), Email.sample());
+        System.out.println(member.toString());
+
+        System.out.println("포인트 10 적립");
+        member.savePoint(new Point(10));
+
+        System.out.println(member.getPoint());
+        System.out.println("포인트 20 적립");
+        member.savePoint(new Point(20));
+        System.out.println(member.getPoint());
+
+        System.out.println("포인트 10 사용");
+        member.usePoint(new Point(10));
+        System.out.println(member.getPoint());
+
+        System.out.println("-----------------------------------------");
+
+    }
 
 }
--- member-ms/src/main/java/com/msa/member/config/KafkaConfig.java
+++ member-ms/src/main/java/com/msa/member/config/KafkaConfig.java
@@ -13,23 +13,23 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
-public class KafkaConfig {
-
-    @Value(value = "${kafka.bootstrapAddress}")
-    private String BOOTSTRAPSERVERS;
-    @Bean
-    public ProducerFactory<String, String> producerFactory()
-    {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAPSERVERS);
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(config);
-    }
-    @Bean
-    public KafkaTemplate kafkaTemplate()
-    {
-        return new KafkaTemplate<>(producerFactory());
-    }
-}
+//@Configuration
+//public class KafkaConfig {
+//
+//    @Value(value = "${kafka.bootstrapAddress}")
+//    private String BOOTSTRAPSERVERS;
+//    @Bean
+//    public ProducerFactory<String, String> producerFactory()
+//    {
+//        Map<String, Object> config = new HashMap<>();
+//        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAPSERVERS);
+//        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+//        return new DefaultKafkaProducerFactory<>(config);
+//    }
+//    @Bean
+//    public KafkaTemplate kafkaTemplate()
+//    {
+//        return new KafkaTemplate<>(producerFactory());
+//    }
+//}
